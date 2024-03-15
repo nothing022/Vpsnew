c6d40068f70db102269c32cd110ac4d0ec85dbf2
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-03-15 10:46:29 +0530

    Update vip_ban.py

--- VIPMUSIC/plugins/sudo/vip_ban.py
+++ VIPMUSIC/plugins/sudo/vip_ban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["ip","iyush"], prefixes=["V", "P"]) & admin_filter)
+@app.on_message(filters.command(["njhhjhbvv","gggffggfc80h"], prefixes=["Vbvvvvvvvcb", bPbvvvvvvv"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
