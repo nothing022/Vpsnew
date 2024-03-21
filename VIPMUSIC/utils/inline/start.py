7b02d6706036ece337119154b03b0c489aac4d0b
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-03-21 16:52:01 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -12,13 +12,13 @@ def start_panel(_):
             ),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="❰ 𝗛𝙴֟፝ؖ۬𝙻𝙿 ❱", callback_data="settings_back_helper"),
             InlineKeyboardButton(
-                text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"
+                text="❰ 𝐒𝙴𝚃 ❱", callback_data="settings_helper"
             ),
         ],
         [
-            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="❰ 𝗚𝚁֟፝ؖ۬𝙾𝚄𝙿 ❱", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -33,11 +33,11 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="❰𝗚𝚁֟፝ؖ۬𝙾𝚄𝙿❱", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="❰𝗠ᴏ֟፝ؖ۬ʀᴇ❱", url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper")
+            InlineKeyboardButton(text="❰ 𝗙𝙴𝙰𝚃֟፝ؖ۬𝚄𝚁𝙴𝚂 ❱", callback_data="settings_back_helper")
         ],
     ]
     return buttons
