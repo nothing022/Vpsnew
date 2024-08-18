bf0d3cb592309cd874acd3908358976990828cd1
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-08-19 00:06:30 +0530

    Update autobroadcast.py

--- VIPMUSIC/plugins/misc/autobroadcast.py
+++ VIPMUSIC/plugins/misc/autobroadcast.py
@@ -44,7 +44,7 @@ async def send_message_to_chats():
             chat_id = chat_info.get('chat_id')
             if isinstance(chat_id, int):  # Check if chat_id is an integer
                 try:
-                    await app.send_photo(chat_id, caption=caption, reply_markup=BUTTON)
+                    await app.send_photo(chat_id, photo=START_IMG_URL, caption=caption, reply_markup=BUTTON)
                     await asyncio.sleep(3)  # Sleep for 100 second between sending messages
                 except Exception as e:
                     pass  # Do nothing if an error occurs while sending message
