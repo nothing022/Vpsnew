ec1632c021af687938506b41b03b2366c7cede3a
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-08-25 22:40:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/RADHAVPS",
+    "https://github.com/SARKAROP123/NEWRADHAVPSAss",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
